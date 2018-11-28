fc0a548323ac9f0abeb31f140be3a5cca2c14e4b
SerksesE <serkses@hotmail.com>
2018-11-28 11:55:00 +0100

    setup

--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -47,8 +47,8 @@ export class Player extends BaseEntity {
   @ManyToOne(_ => Game, game => game.players)
   game: Game
 
-  @Column()
-  userId: number
+  // @Column()
+  // userId: number
 
   @Column('char', {length: 1})
   symbol: Symbol
